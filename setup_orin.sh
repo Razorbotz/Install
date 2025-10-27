3885ef34ad616f237914e20ffcf3c1bbe777e72a
Andrew Burroughs <43488350+andrewburroughs@users.noreply.github.com>
2025-10-27 01:47:51 -0500

    Updating to Ubuntu22.04

--- setup_orin.sh
+++ setup_orin.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e 
 
 mkdir -p ~/SoftwareDevelopment/ROS2
 cd ~/SoftwareDevelopment/ROS2
@@ -25,15 +24,11 @@ cd ../..
 
 mv SoftwareDevelopment ../
 
-if [ -d "./ctre" ]; then
-    sudo mv ./ctre /usr/local/include || true
-fi
+sudo mv ./ctre /usr/local/include || true
+sudo mv ./lib/aarch64/* /usr/local/lib/ || true
 
-if [ -d "./lib/x86_64" ]; then
-    sudo mv ./lib/x86_64/* /usr/local/lib/ || true
-fi
-
-sudo apt update && sudo apt install -y locales
+locale
+sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -42,19 +37,15 @@ locale  # verify settings
 sudo apt install -y software-properties-common curl gnupg lsb-release
 sudo add-apt-repository universe -y
 
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
-  -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) \
-signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
-http://packages.ros.org/ros2/ubuntu \
-$(. /etc/os-release && echo $UBUNTU_CODENAME) main" \
-| sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
 
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y ros-humble-desktop ros-dev-tools
+sudo apt install ros-humble-desktop ros-dev-tools
 
 if ! grep -q "source /opt/ros/humble/setup.bash" ~/.bashrc; then
     echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
@@ -66,10 +57,22 @@ if ! grep -q "GAZEBO_MODEL_PATH" ~/.bashrc; then
     echo "export GAZEBO_MODEL_PATH=\$HOME/SoftwareDevelopment/ROS2/shovel/src/sim/models:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
 fi
 
+# Install Gazebo
+sudo apt-get update
+sudo apt-get install curl lsb-release gnupg
+
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] https://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install ignition-fortress
+
 sudo apt install -y ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros2-control
 sudo apt install -y ros-humble-rtabmap-ros
 
-sudo apt install -y gz-garden || sudo apt install -y gz-fortress
+sudo apt install ros-humble-ros-gz-plugins
+sudo apt install ros-humble-ros-gz-sim
+sudo apt install ros-humble-ros-gz-bridge
+sudo apt install ros-humble-rtabmap-gz
 
 sudo chmod +x install_gui_deps.sh
 ./install_gui_deps.sh
--- setup_wsl.sh
+++ setup_wsl.sh
@@ -33,35 +33,51 @@ sudo mv ./lib/x86_64/* /usr/local/lib/
 
 # Install ROS2
 locale
-
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-
 locale  # verify settings
 
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+sudo apt install -y software-properties-common curl gnupg lsb-release
+sudo add-apt-repository universe -y
 
-sudo apt update && sudo apt install curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
 
 sudo apt update
-sudo apt upgrade
-sudo apt install ros-galactic-desktop
+sudo apt upgrade -y
 
-sudo apt install ros-dev-tools
+sudo apt install ros-humble-desktop ros-dev-tools
 
-echo "source /opt/ros/galactic/setup.bash" >> ~/.bashrc
-echo "source ~/SoftwareDevelopment/C++/robotcontrollerclient/control_completion.sh" >> ~/.bashrc
-echo "export GAZEBO_MODEL_PATH=/home/team/SoftwareDevelopment/ROS2/shovel/src/sim/models:$GAZEBO_MODEL_PATH" >> ~/.bashrc
+if ! grep -q "source /opt/ros/humble/setup.bash" ~/.bashrc; then
+    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
+if ! grep -q "control_completion.sh" ~/.bashrc; then
+    echo "source ~/SoftwareDevelopment/C++/robotcontrollerclient/control_completion.sh" >> ~/.bashrc
+fi
+if ! grep -q "GAZEBO_MODEL_PATH" ~/.bashrc; then
+    echo "export GAZEBO_MODEL_PATH=\$HOME/SoftwareDevelopment/ROS2/shovel/src/sim/models:\$GAZEBO_MODEL_PATH" >> ~/.bashrc
+fi
 
-sudo apt-get install gazebo11
-sudo apt install ros-galactic-gazebo-plugins
-sudo apt install ros-galactic-rtabmap-ros
+# Install Gazebo
+sudo apt-get install curl lsb-release gnupg
+
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] https://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install ignition-fortress
+
+sudo apt install -y ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros2-control
+sudo apt install -y ros-humble-rtabmap-ros
+
+sudo apt install ros-humble-ros-gz-plugins
+sudo apt install ros-humble-ros-gz-sim
+sudo apt install ros-humble-ros-gz-bridge
+sudo apt install ros-humble-rtabmap-gz
+
 
 sudo chmod +x install_gui_deps.sh
 ./install_gui_deps.sh
